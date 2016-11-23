13c971eed016f3c64b884a8a2ea841d84fc7c7a7
Will <leo0908w@yahoo.com.tw>
2016-11-23 09:30:39 +0800

    v2

--- app/src/main/java/com/org/iii/will12/MainActivity.java
+++ app/src/main/java/com/org/iii/will12/MainActivity.java
@@ -1,9 +1,15 @@
 package com.org.iii.will12;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,7 +20,11 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.HttpURLConnection;
@@ -22,12 +32,17 @@ import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.Socket;
 import java.net.URL;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private UIHandler uiHandler;
     private ImageView imageView;
     private Bitmap bmp;
+    private String urlDownload;
+    private File sdroot;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +52,31 @@ public class MainActivity extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.image);
         textView = (TextView) findViewById(R.id.textView);
         uiHandler = new UIHandler();
+
+        // 1. 權限
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            // request permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+        }
+
+        // 2. 環境
+        init();
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        init();
+    }
+
+    private void init() {
+        sdroot = Environment.getExternalStorageDirectory();
+
+    }
     // UDP Sender
     public void test1(View v){
 
@@ -126,8 +164,6 @@ public class MainActivity extends AppCompatActivity {
                 } catch (Exception e) {
                     Log.v("will", e.toString());
                 }
-
-
             }
         }.start();
     }
@@ -146,11 +182,64 @@ public class MainActivity extends AppCompatActivity {
                     uiHandler.sendEmptyMessage(2);
 
                 }catch (Exception e){
-                    Log.v("brad", e.toString());
+                    Log.v("will", e.toString());
                 }
             }
         }.start();
     }
+
+    public void test6(View v){
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    MultipartUtility mu = new MultipartUtility("http://10.0.3.2/add2.php", "UTF-8");
+                    mu.addFormField("account", "mark");
+                    mu.addFormField("password", "654321");
+                    List<String> ret = mu.finish();
+                    Log.v("will", ret.get(0));
+                }catch (Exception e){
+                    Log.v("will", e.toString());
+                }
+            }
+        }.start();
+    }
+
+    // Download save to SDCard
+    public void test7(View v){
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL("http://pdfmyurl.com/?url=" + urlDownload);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.connect();
+
+                    InputStream in = conn.getInputStream();
+
+                    File download = new File(sdroot, "will.pdf");
+                    FileOutputStream fout = new FileOutputStream(download);
+                    byte[] buf = new byte[4096];    int len;
+                    while ((len = in.read()) != -1) {
+
+                    }
+
+                }catch (Exception e){
+                    Log.v("will", "test7" + e.toString());
+                }
+            }
+        }.start();
+    }
+
+    // Upload to My Server => add2.php
+    public void test8(View v){
+        new Thread(){
+            @Override
+            public void run() {
+            }
+        }.start();
+    }
+
 
     private class UIHandler extends Handler {
         @Override
